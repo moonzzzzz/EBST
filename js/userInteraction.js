25c4d563377ea42bbf99bffbae131cfd4374979a
moonza@something.com <moonza@something.com>
2019-12-01 08:49:18 +0000

    feature: found a way to append elements to the dropdown list

--- js/userInteraction.js
+++ js/userInteraction.js
@@ -3,12 +3,19 @@
 function addAction() {
     // name = read the drop down menu
 
+    actionName = document.getElementById("adding-action").innerHTML;
     mainAddAction(actionName);
 }
 
 function mainAddAction(name) {
     // actual add action function
     // should be a drop down list of 4 actions
+
+    var node = document.createElement("option");
+    var textnode = document.createTextNode("Water");
+    node.appendChild(textnode);
+    document.getElementById("adding-sensors").appendChild(node);
+    console.log(name)
 }
 
 function updateBehaviourModel() {
