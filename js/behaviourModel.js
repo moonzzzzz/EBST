08172a9acb778c3ba3b196f96c05df5fb1f0a339
moonza@something.com <moonza@something.com>
2019-12-03 07:21:55 +0000

    feature: adding sensor to model complete

--- js/behaviourModel.js
+++ js/behaviourModel.js
@@ -1,16 +1,17 @@
 // // Graph: create an array of objects for actions, with child sensors - also an array of objects
 let actions = {
-    move:{name: "MOVE", "sensors": [0, 1]},
+    move:{name: "MOVE", "sensors": []},
     extend:{name: "EXTEND", "sensors": []},
     climb_on:{name: "CLIMB_ON", "sensors": [3]}, 
     climb_off:{name: "CLIMB_OFF", "sensors": []}, 
     dead:{name: "DEAD", "sensors": []}
 }
 
-let sensors = [{"id": 0, type: "EDGE", probs:[0.2, 0.8], actions:[actions.extend, actions.move]},
-    {"id": 1, type: "ANT_EXTENDING", probs:[.9, .1], actions:[actions.climb_on, actions.move]},
-    {"id": 2, type: "TIME", probs:[1, 0], actions:[actions.climb_off, actions.extend]},
-    {"id": 3, type: "TIME", probs:[0.5, 0.5], actions:[actions.climb_off, actions.climb_on]}
+let sensors = [
+    // {"id": 0, type: "EDGE", probs:[0.2, 0.8], actions:[actions.extend, actions.move]},
+    // {"id": 1, type: "ANT_EXTENDING", probs:[.9, .1], actions:[actions.climb_on, actions.move]},
+    // {"id": 2, type: "TIME", probs:[1, 0], actions:[actions.climb_off, actions.extend]},
+    // {"id": 3, type: "TIME", probs:[0.5, 0.5], actions:[actions.climb_off, actions.climb_on]}
 ]
 
 let priorities = ["TIME", "EDGE", "ANT_EXTENDING"];
--- js/userInteraction.js
+++ js/userInteraction.js
@@ -34,8 +34,8 @@ function mainAddAction(action) {
     document.getElementById("add-sensor-actions").appendChild(node);
 
     // adding to the add-sensor-actions list
-    var node = document.createElement("option");
-    var textnode = document.createTextNode(action.options[action.selectedIndex].text);
+    node = document.createElement("option");
+    textnode = document.createTextNode(action.options[action.selectedIndex].text);
     node.appendChild(textnode);
     document.getElementById("connect-sensor-actions").appendChild(node);
     
@@ -52,8 +52,36 @@ function mainAddSensor(action, sensor) {
     node.appendChild(textnode);
     document.getElementById("connect-sensor-sensors").appendChild(node);
 
-    // ToDo: adding to the behaviour model
-    
+    // // Add item to the behaviour model
+
+    // obtain new sensor parameters (id, type, probs, actions)
+    let newId = sensors.length;     // one greater than last id
+    let newType = sensor.options[sensor.selectedIndex].text.toUpperCase();
+    let newProbs = [1];  // loop back to the initial action
+    let newActions;
+
+    // add the new sensor to the action
+    let actionName = action.options[action.selectedIndex].text.toLowerCase();
+    if(actionName == "move"){
+        actions.move.sensors.push(newId);
+        newActions = [actions.move];    // loop back to the initial action
+        console.log(actions.move);
+    } else if(actionName == "extend"){
+        newActions = [actions.extend];    // loop back to the initial action
+        console.log(actions.extend);
+    } else if(actionName == "climb on"){
+        newActions = [actions.climb_on];    // loop back to the initial action
+        console.log(actions.climb_on);
+    } else if(actionName == "climb off"){
+        newActions = [actions.climb_off];    // loop back to the initial action
+        console.log(actions.climb_off);
+    } else {console.log("ERROR");}
+
+    // input into new variable -> into sensors
+    let newSensor = {id: newId, type: newType, probs: newProbs, actions: newActions};
+    sensors.push(newSensor);
+    console.log(sensors);
+
 }
 
 function updateBehaviourModel() {
